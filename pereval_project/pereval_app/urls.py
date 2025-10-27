26257952f015ff7762c7d0df3f377674d66b370e
Mihail <obyhvostov@gmail.com>
2025-10-27 23:00:34 +0300

    Revert "per_ver_0.4"
    This reverts commit 87a45a9bb7034da2da4f6a4f20fe72d5e093db2f.

--- pereval_project/pereval_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('submitData/', views.submit_data, name='submit_data'),
-    path('submitData/<int:id>/', views.get_pereval_by_id, name='get_pereval_by_id'),
-    path('submitData/<int:id>/', views.edit_pereval, name='edit_pereval'),
-    path('submitData/', views.get_perevals_by_user_email, name='get_perevals_by_user_email'),
-]
